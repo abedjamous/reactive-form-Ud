5abda96ea7159e16950c26289e79f526deb2b8d5
Abed Jamous <abedjamous@Abeds-MacBook-Pro.local>
2021-08-20 23:06:07 -0500

    reactive form example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form-Ud';
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm!: FormGroup;
+
+  constructor(){
+    
+  }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null,[Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm)
+  }
 }
