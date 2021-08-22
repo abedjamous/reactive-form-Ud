805fb457152b5102005e5873ca733827774c8d64
Abed Jamous <abedjamous@Abeds-MacBook-Pro.local>
2021-08-21 22:55:30 -0500

    form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -20,11 +20,17 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null,[Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies' : new FormArray ([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm)
+  }
+
+  get controls() {
+    const control = new FormControl(null, Validators.required);
+    return (this.signupForm.get('hobbies') as FormArray).push(control);
   }
 }
